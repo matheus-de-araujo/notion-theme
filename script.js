33b6bdf1c9c98d6e52842bb774c85bb483805fb4
Matheus de Araujo <matheus@pop-os.localdomain>
2021-09-04 00:22:34 -0300

    Commit initial

---
+++ script.js
@@ -0,0 +1,19 @@
+// function calc() {
+//     var hourStart = new Date(document.getElementById('calc-hour-start-date').value);
+//     var hourFinish = new Date(document.getElementById('calc-hour-finish-date').value);
+
+//     var hourResult = new Date (hourFinish - hourStart);
+    
+//     console.log(hourStart);
+//     console.log(hourFinish);
+//     console.log(hourResult);
+
+//     document.getElementById('calc-hour').innerHTML = hourResult.value;
+
+// }
+
+// var button = document.getElementById('calc-hour-button');
+
+// button.addEventListener("click", calc);
+
+console.log("ola");
