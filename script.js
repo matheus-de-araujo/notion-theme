6c13ac53034bcb70f37b676f14406ca4f056a664
Matheus de Araujo <matheus@pop-os.localdomain>
2021-09-29 14:44:51 -0300

    Correçoes no projeto

--- script.js
+++
@@ -1,7 +0,0 @@
-let media = document.body.querySelectorAll("img, picture, video");
-
-media.forEach((mediaItem) => {
-    mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
-})
-
-document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
